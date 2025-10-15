912d281494e28c572dd0cf5235beaeec84e0cd52
marathon <QSfTfSilinaRoza@gmail.com>
2025-10-15 14:52:17 +0300

    Fix cms frontend preview

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -62,13 +62,13 @@ class Home extends BaseController
 
 		echo view('v2/pages/home', $this->data); // after you stored the query results inside the $data array, send the array to the view 
 	}
-
+  
 	function preview() {
 		$Admin = new Admin();
 		$Product = new Product();
 
 		$img_host = $Admin->get_plink('img_host');
-		$user_info = new \stdClass();
+		$user_info = false;//new \stdClass();
 
 		// branch
 		session()->set('branch', request()->getGet('branch'));
@@ -78,28 +78,83 @@ class Home extends BaseController
 
 		// price_list
 		$price_list = explode(',', request()->getGet('price_list'));
-		$user_info->price_list001 = in_array('001', $price_list) ? 1 : 0;
-		$user_info->price_list005 = in_array('005', $price_list) ? 1 : 0;
-		$user_info->price_list007 = in_array('007', $price_list) ? 1 : 0;
-		$user_info->price_list008 = in_array('008', $price_list) ? 1 : 0;
-		$user_info->price_list009 = in_array('009', $price_list) ? 1 : 0;
-		$user_info->price_list010 = in_array('010', $price_list) ? 1 : 0;
-		$user_info->price_list011 = in_array('011', $price_list) ? 1 : 0;
-		$user_info->price_list012 = in_array('012', $price_list) ? 1 : 0;
-		$user_info->price_list999 = in_array('999', $price_list) ? 1 : 0;
+		// $user_info->price_list001 = in_array('001', $price_list) ? 1 : 0;
+		// $user_info->price_list005 = in_array('005', $price_list) ? 1 : 0;
+		// $user_info->price_list007 = in_array('007', $price_list) ? 1 : 0;
+		// $user_info->price_list008 = in_array('008', $price_list) ? 1 : 0;
+		// $user_info->price_list009 = in_array('009', $price_list) ? 1 : 0;
+		// $user_info->price_list010 = in_array('010', $price_list) ? 1 : 0;
+		// $user_info->price_list011 = in_array('011', $price_list) ? 1 : 0;
+		// $user_info->price_list012 = in_array('012', $price_list) ? 1 : 0;
+		// $user_info->price_list999 = in_array('999', $price_list) ? 1 : 0;
 
 		// date
 		$date = request()->getGet('date');
 
-		$this->data['top_ribbon'] = Cms::getActiveTopRibbon($date);
-		$this->data['products_carousels'] = Cms::getActiveProductsCarousels($user_info, $this->priceList, $img_host, $this, $date);
-		$this->data['brochures'] = Cms::getActiveBrochures($date);
-		$this->data['home_banners'] = Cms::getActiveHomeBanners($date);
-		$this->data['bottom_banners'] = Cms::getActiveBottomBanners($date);
-		$this->data['brands'] = Cms::getActiveBrands($date);
+		$this->data["link1"]    = $Admin->get_plink('link newsletter');	
+		$this->data["link2"]    = $Admin->get_plink('link cash & carry');	
+		$this->data["link3"]    = $Admin->get_plink('link day-today');	
+		$this->data["link3a"]   = $Admin->get_plink('link day-today upcoming');	
+		$this->data["link4"]    = $Admin->get_plink('link usave');	
+		$this->data["link4a"]   = $Admin->get_plink('link usave upcoming');	
+		$this->data["link5"]    = $Admin->get_plink('link special event');		
+		$this->data["period1"]  = $Admin->get_plink('link newsletter period');	
+		$this->data["period2"]  = $Admin->get_plink('link cash & carry period');	
+		$this->data["period3"]  = $Admin->get_plink('link day-today period');	
+		$this->data["period3a"] = $Admin->get_plink('link day-today upcoming period');	
+		$this->data["period4"]  = $Admin->get_plink('link usave period');	
+		$this->data["period4a"] = $Admin->get_plink('link usave upcoming period');	
+		$this->data["period5"]  = $Admin->get_plink('link special event period');						
+		$this->data["switch"]   = $Admin->get_plink('state special event');			
+		$this->data["switch2"]  = $Admin->get_plink('state newsletter');	
+		$this->data["slides"]   = $Admin->get_scount('slides');		
+		$this->data["sponsors"] = $Admin->get_scount('sponsors');
+		$this->data["user_info"] = $user_info;
+		$router = service('router');
 
-		echo view('home', $this->data); // after you stored the query results inside the $data array, send the array to the view 
+		$this->data['cms'] = Cms::getHomeCmsByPagePosition($user_info, $this->priceList, $img_host, $this, $date);
+
+		echo view('v2/pages/home', $this->data); // after you stored the query results inside the $data array, send the array to the view 
 	}
+
+
+	// function preview() {
+	// 	$Admin = new Admin();
+	// 	$Product = new Product();
+
+	// 	$img_host = $Admin->get_plink('img_host');
+	// 	$user_info = new \stdClass();
+
+	// 	// branch
+	// 	session()->set('branch', request()->getGet('branch'));
+
+	// 	// organization
+	// 	session()->set('organization', request()->getGet('organization'));
+
+	// 	// price_list
+	// 	$price_list = explode(',', request()->getGet('price_list'));
+	// 	$user_info->price_list001 = in_array('001', $price_list) ? 1 : 0;
+	// 	$user_info->price_list005 = in_array('005', $price_list) ? 1 : 0;
+	// 	$user_info->price_list007 = in_array('007', $price_list) ? 1 : 0;
+	// 	$user_info->price_list008 = in_array('008', $price_list) ? 1 : 0;
+	// 	$user_info->price_list009 = in_array('009', $price_list) ? 1 : 0;
+	// 	$user_info->price_list010 = in_array('010', $price_list) ? 1 : 0;
+	// 	$user_info->price_list011 = in_array('011', $price_list) ? 1 : 0;
+	// 	$user_info->price_list012 = in_array('012', $price_list) ? 1 : 0;
+	// 	$user_info->price_list999 = in_array('999', $price_list) ? 1 : 0;
+
+	// 	// date
+	// 	$date = request()->getGet('date');
+
+	// 	$this->data['top_ribbon'] = Cms::getActiveTopRibbon($date);
+	// 	// $this->data['products_carousels'] = Cms::getActiveProductsCarousels($user_info, $this->priceList, $img_host, $this, $date);
+	// 	// $this->data['brochures'] = Cms::getActiveBrochures($date);
+	// 	// $this->data['home_banners'] = Cms::getActiveHomeBanners($date);
+	// 	// $this->data['bottom_banners'] = Cms::getActiveBottomBanners($date);
+	// 	// $this->data['brands'] = Cms::getActiveBrands($date);
+
+	// 	echo view('home', $this->data); // after you stored the query results inside the $data array, send the array to the view 
+	// }
 
 	function mobile() {
 		$is_mobile = request()->getPost('is_mobile');
